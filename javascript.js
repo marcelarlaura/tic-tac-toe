30b274b56e48bcaf29824981532fc0f2792ae21e
marcelarlaura <171378249+marcelarlaura@users.noreply.github.com>
2025-02-16 11:28:07 -0500

    make cells fill in only once with X or O

--- javascript.js
+++ javascript.js
@@ -6,26 +6,54 @@ let message = document.querySelector('.message');
 
 function startGame(){
     let arrayMark = [];
-    let assignId1 = 0;
-    let assignId2 = 0;
+    let assignId1 = 1;
+    let assignId2 = 1;
     let turn = 0;
 
-    for (let i=0; i<9; i++){
+    gameboard.innerHTML = '';
+
+    for (let i=1; i<10; i++){
+        arrayMark[i-1] = `${assignId2}`;
+        assignId2++;
+    }
+
+    for (let i=1; i<10; i++){
         const cell = document.createElement('button');
         cell.id= `${assignId1}`;
         assignId1++;
         cell.textContent = cell.id;
         gameboard.append(cell);
-        cell.addEventListener('click', markCell());
+        cell.addEventListener('click', ()=>{
+            if (turn % 2 == 0){
+                
+                cell.textContent = 'X';
+                cell.disabled = true;
+
+                for (let i=1;i<10; i++){
+                    if (arrayMark[i-1] == cell.id){
+                        arrayMark[i-1] = 'X';
+                    }
+                }
+
+            } else {
+                
+                cell.textContent= 'O';
+                cell.disabled =true;
+                
+                for (let i=1;i<10; i++){
+                    if (arrayMark[i-1] == cell.id){
+                        arrayMark[i-1] = 'O';
+                    }
+                }
+            }
+
+            turn++;
+            
+        })
+
     }
-    for (let i=0; i<9; i++){
-        arrayMark[i] = `${assignId2}`;
-        assignId2++;
-    }
+
     
 }
 
-function markCell(){
-    
-}
 

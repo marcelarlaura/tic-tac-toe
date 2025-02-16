4a2ab6de5e192406d39f854d270d718f5fbae154
marcelarlaura <171378249+marcelarlaura@users.noreply.github.com>
2025-02-16 11:59:38 -0500

    add structure to decided who won or if there was a tie

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 let newGame = document.querySelector('.newGame');
 let gameboard = document.querySelector('.gameboard');
 let message = document.querySelector('.message');
+let announcement = document.querySelector('.winner-announcement');
 
 
 
@@ -12,6 +13,8 @@ function startGame(){
 
     gameboard.innerHTML = '';
 
+    message.textContent = "It's Player X's turn!";
+    announcement.textContent = '';
     for (let i=1; i<10; i++){
         arrayMark[i-1] = `${assignId2}`;
         assignId2++;
@@ -35,6 +38,8 @@ function startGame(){
                     }
                 }
 
+                message.textContent = "It's Player O's turn!";
+
             } else {
                 
                 cell.textContent= 'O';
@@ -45,11 +50,48 @@ function startGame(){
                         arrayMark[i-1] = 'O';
                     }
                 }
+                message.textContent = "It's Player X's turn!";
             }
 
             turn++;
+
+            checkWinner();
             
         })
+
+        function checkWinner (){
+            if (arrayMark[0] == arrayMark[1] && arrayMark[1] == arrayMark[2]){
+                announcement.textContent = `HURRAY! PLAYER ${arrayMark[1]} HAS WON!`;
+            } else if (arrayMark[3] == arrayMark[4] && arrayMark[4] == arrayMark[5]){
+                announcement.textContent = `HURRAY! PLAYER ${arrayMark[4]} HAS WON!`;
+            } else if (arrayMark[0] == arrayMark[4] && arrayMark[4] == arrayMark[8]){
+                announcement.textContent = `HURRAY! PLAYER ${arrayMark[4]} HAS WON!`;
+            } else if (arrayMark[6] == arrayMark[4] && arrayMark[4] == arrayMark[2]){
+                announcement.textContent = `HURRAY! PLAYER ${arrayMark[4]} HAS WON!`;
+            } else if (arrayMark[0] == arrayMark[3] && arrayMark[3] == arrayMark[6]){
+                announcement.textContent = `HURRAY! PLAYER ${arrayMark[3]} HAS WON!`;
+            } else if (arrayMark[1] == arrayMark[4] && arrayMark[4] == arrayMark[7]) {
+                announcement.textContent = `HURRAY! PLAYER ${arrayMark[4]} HAS WON!`;
+            } else if (arrayMark[2] == arrayMark[5] && arrayMark[5] == arrayMark[8]) {
+                announcement.textContent = `HURRAY! PLAYER ${arrayMark[5]} HAS WON!`;
+            } else if (arrayMark[6] == arrayMark[7] && arrayMark[7] == arrayMark[8]) {
+                announcement.textContent = `HURRAY! PLAYER ${arrayMark[7]} HAS WON!`;
+            } else {
+
+                let checked = 0;
+                for (let i=0; i<9; i++){
+                    if (arrayMark[i] == 'O' || arrayMark[i] == 'X'){
+                        checked++;
+                    }
+                }
+                if (checked > 7){
+                    announcement.textContent = "IT'S A TIE!";
+                }
+
+                return checked;
+                
+            }
+        }
 
     }
 
